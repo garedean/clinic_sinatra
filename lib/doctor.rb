c14234cebc8cb959d73f2089f155e2cbc3a436f3
James Williams & Garrett Olson <student@epicodus.com>
2015-05-05 12:25:15 -0700

    update ruby logic to include save, all, and equal method for doctors

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,3 +1,36 @@
 class Doctor
+  attr_reader(:first_name, :last_name, :id, :specialty_id)
 
+  def initialize(attributes)
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
+    @id = attributes[:id]
+    @specialty_id = attributes[:specialty_id]
+  end
+
+  def save
+    result = DB.exec("INSERT INTO doctors (first_name, last_name, specialty_id) VALUES ('#{@first_name}', '#{@last_name}', #{@specialty_id}) RETURNING id;")
+    @id = result.first["id"].to_i
+  end
+
+  def self.all
+    returned_doctors = DB.exec("SELECT * FROM doctors")
+    doctors = []
+    returned_doctors.each do |doctor|
+      id           = doctor['id'].to_i()
+      first_name   = doctor['first_name']
+      last_name    = doctor['last_name']
+      specialty_id = doctor['specialty_id'].to_i()
+      doctors << Doctor.new(id: id,
+                            first_name: first_name,
+                            last_name: last_name,
+                            specialty_id: specialty_id)
+    end
+    doctors
+  end
+
+  def ==(another_doctor)
+    self.last_name == another_doctor.last_name && self.specialty_id == another_doctor.specialty_id
+
+  end
 end
--- spec/doctor_spec.rb
+++ spec/doctor_spec.rb
@@ -7,4 +7,51 @@ describe(Doctor) do
       expect(doctor.first_name).to(eq("Bill"))
     end
   end
+
+  describe('#last_name') do
+    it("returns the doctor's first name") do
+      doctor = Doctor.new(id: nil, first_name: "Bill", last_name: "Brasky", specialty_id: 1)
+      expect(doctor.last_name).to(eq("Brasky"))
+    end
+  end
+
+  describe('#id') do
+    it("returns the doctor's first name") do
+      doctor = Doctor.new(id: nil, first_name: "Bill", last_name: "Brasky", specialty_id: 1)
+      doctor.save()
+      expect(doctor.id).to(be_an_instance_of(Fixnum))
+    end
+  end
+
+  describe('#specialty_id') do
+    it("returns the doctor's first name") do
+      doctor = Doctor.new(id: nil, first_name: "Bill", last_name: "Brasky", specialty_id: 1)
+      expect(doctor.specialty_id).to(eq(1))
+    end
+  end
+
+  describe('#save') do
+    it("saves the doctor to doctor list") do
+      doctor = Doctor.new(id: nil, first_name: "Bill", last_name: "Brasky", specialty_id: 1)
+      doctor.save()
+      expect(Doctor.all()).to(eq([doctor]))
+    end
+  end
+
+  describe('.all') do
+    it("is empty at first") do
+      expect(Doctor.all()).to(eq([]))
+    end
+  end
+
+  describe('#==') do
+    it("is equal if doctor first name, last name, and specialty id is the same") do
+      doctor = Doctor.new(id: nil, first_name: "Bill", last_name: "Brasky", specialty_id: 1)
+      doctor.save()
+      doctor1 = Doctor.new(id: nil, first_name: "Bill", last_name: "Brasky", specialty_id: 1)
+      doctor1.save()
+      expect(doctor1).to(eq(doctor))
+    end
+  end
+
 end
